f986f070fbca93e9bcc0a5a06d1892d999cbe127
sknat <Sknastko@gmail.com>
2024-02-05 16:55:35 -0500

    first commit

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,14 @@
-public class Animal {
+public class Animal implements Info {
+    public int id;
+
+    public Animal(int id) {
+        this.id = id;
+    }
+
+    public void sleep(){
+        System.out.println("i am sleeping");
+    }
+    public void Showinfo() {
+        System.out.println("id is: "+this.id);
+    }
 }
